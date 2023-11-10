40eb8988ad7acc29835a30d70d514f23215862d2
Ivan Pianetti <ivanpianetti23@gmail.com>
2023-11-10 20:04:48 -0300

    Added createCustomer validations

--- src/createCustomer.ts
+++ src/createCustomer.ts
@@ -12,10 +12,27 @@ import DynamoDBClient from './services/dynamodb';
  */
 module.exports.createCustomer = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const body: Customer = JSON.parse(event.isBase64Encoded ? Buffer.from(event.body, 'base64').toString() : event.body);
+
+  if (!body?.email || !body?.name) {
+    return new ResponseData({error: 'Email and name are required.'}, StatusCode.CONFLICT);
+  }
+  body.email = body?.email?.toLowerCase();
+
+  const existCustomer = await DynamoDBClient.get({
+    TableName: process.env.DYNAMODB_CUSTOMER_TABLE,
+    Key: {
+      email: body.email,
+    },
+    ProjectionExpression: 'email',
+  });
+  if (existCustomer.Item) {
+    return new ResponseData({error: `Customer '${body.email}' already created.`}, StatusCode.CONFLICT);
+  }
+
   const putParams: PutCommandInput = {
     TableName: process.env.DYNAMODB_CUSTOMER_TABLE,
     Item: {
-      email: body.email?.toLowerCase(),
+      email: body.email,
       name: body.name,
     },
   };
